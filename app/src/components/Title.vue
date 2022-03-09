c633c6a624917af710166e81aea3178db09a51fc
Jozias Martini <jozias.martini@gmail.com>
2022-03-08 23:19:03 -0300

    Create title component

---
+++ app/src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="title">{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Title',
+  props: {
+    title: String,
+  },
+});
+</script>
+
+<style scoped lang="sass">
+.title
+  font-size: 40px
+</style>
