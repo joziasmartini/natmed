2fa3c8d909b93d682242e797ae0f3c545a0f9b89
Jozias Martini <jozias.martini@gmail.com>
2022-03-08 23:30:42 -0300

    Create button component

---
+++ app/src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <a
+    href="#"
+    :class="{
+      button: true,
+      primary: primary,
+      secondary: secondary
+    }">
+    {{ text }}
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+  props: {
+    text: String,
+    primary: Boolean,
+    secondary: Boolean,
+  },
+});
+</script>
+
+<style scoped lang="sass">
+.button
+  display: block
+  margin: 0 auto
+  font-size: 14px
+  padding: 10px
+  max-width: 300px
+  color: white
+  border-radius: 50px
+  text-decoration: none
+
+.primary
+  background: green
+
+.secondary
+  background: white
+</style>
