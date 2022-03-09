747b1cc69d4082f89ba9c110a53ec8b16f3416e0
Jozias Martini <jozias.martini@gmail.com>
2022-03-08 23:34:27 -0300

    Create logo component

---
+++ app/src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Logo',
+});
+</script>
+
+<style scoped lang="sass">
+
+</style>
