3db53e19cc66b3eb7c658ca733315cb0b6b91c71
Jozias Martini <jozias.martini@gmail.com>
2022-03-08 23:34:35 -0300

    Create input component

---
+++ app/src/components/Input.vue
@@ -0,0 +1,27 @@
+<template>
+  <label :for="id">
+    {{ labelText }}
+    <input
+      :value="modelValue"
+      @change="$emit('update:modelValue', $event.target.value)"
+      :id="id"
+    />
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Input',
+  props: {
+    id: String,
+    labelText: String,
+    modelValue: String,
+  },
+});
+</script>
+
+<style scoped lang="sass">
+
+</style>
