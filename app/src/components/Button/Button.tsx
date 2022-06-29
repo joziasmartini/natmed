3baf9ea9ce03c25fda7f06da187f4d84f281ff5c
Jozias Martini <jozias.martini@gmail.com>
2022-06-29 16:13:57 -0300

    Create first action for Button component

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -3,8 +3,17 @@ import "./Button.sass";
 type Props = {
   text: string;
   type: "primary" | "secondary" | "danger";
+  action: "login";
 };
 
 export default function Button({ text, type }: Props) {
-  return <div className={`button ${type}`}>{text}</div>;
+  const clickHandle = () => {
+    console.log("Clicked");
+  };
+
+  return (
+    <div className={`button ${type}`} onClick={() => clickHandle()}>
+      {text}
+    </div>
+  );
 }
