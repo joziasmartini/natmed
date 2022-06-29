0cfd6f26e716b949426ab6be7cf90439fad61696
Jozias Martini <jozias.martini@gmail.com>
2022-06-29 16:14:21 -0300

    Change App structure with Logo and Button

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,16 @@
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
 import Interface from "./components/Interface/Interface";
+import Logo from "./components/Logo/Logo";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Interface>
+      <Logo />
       <Input type="text" placeholder="Email" />
       <Input type="password" placeholder="Password" />
-      <Button type="primary" text="Login" />
+      <Button type="primary" text="Login" action="login" />
       <Navbar theme="light" />
     </Interface>
   );
