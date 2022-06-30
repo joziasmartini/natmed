c07031e88db275e9127297d455ac4183f665cac0
Jozias Martini <jozias.martini@gmail.com>
2022-06-30 01:39:14 -0300

    Create user data state and login handle in App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,15 +5,30 @@ import Version from "./components/Version/Version";
 import Input from "./components/Input/Input";
 import Button from "./components/Button/Button";
 import Navbar from "./components/Navbar/Navbar";
+import Space from "./components/Space/Space";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const clickHandle = (type: string) => {
+    if (type === "login") {
+      if (email === "jozias.martini@gmail.com" && password === "12345") {
+        console.log("User logged in.");
+      }
+    }
+  };
+
   return (
     <Interface>
+      <Space pixels={120} />
       <Logo />
       <Version info={Package.version} />
-      <Input type="text" placeholder="Email" />
-      <Input type="password" placeholder="Password" />
-      <Button type="primary" text="Login" action="login" />
+      <Input type="text" placeholder="Email" action={setEmail} />
+      <Input type="password" placeholder="Password" action={setPassword} />
+      <Button type="primary" text="Login" action={clickHandle} />
+      <Space pixels={120} />
       <Navbar theme="light" />
     </Interface>
   );
