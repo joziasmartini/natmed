1b521e1299deeb69a083c0b66b339e5ede8b64b4
Jozias Martini <jozias.martini@gmail.com>
2022-06-30 00:29:04 -0300

    Restructure importings and add Version to App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,16 @@
-import Button from "./components/Button/Button";
-import Input from "./components/Input/Input";
+import Package from "../package.json";
 import Interface from "./components/Interface/Interface";
 import Logo from "./components/Logo/Logo";
+import Version from "./components/Version/Version";
+import Input from "./components/Input/Input";
+import Button from "./components/Button/Button";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Interface>
       <Logo />
+      <Version info={Package.version} />
       <Input type="text" placeholder="Email" />
       <Input type="password" placeholder="Password" />
       <Button type="primary" text="Login" action="login" />
