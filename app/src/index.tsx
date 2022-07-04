75cdfd0f9ab1e4042891d90966e0946608d4a38a
Jozias Martini <jozias.martini@gmail.com>
2022-07-04 12:40:08 -0300

    Add react-router to index and declare paths

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { 
+  BrowserRouter, 
+  Routes,
+  Route, 
+} from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./Global.sass";
+import Search from "./views/Search";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
