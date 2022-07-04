427982cb5f84ff0d972b1ab55707b3ebe8f5ba0c
Jozias Martini <jozias.martini@gmail.com>
2022-07-04 13:26:30 -0300

    Delete Space component

--- app/src/components/Space/Space.tsx
+++
@@ -1,9 +0,0 @@
-import "./Space.sass";
-
-type Props = {
-  pixels: number;
-};
-
-export default function Space({ pixels }: Props) {
-  return <div className="space" style={{ height: pixels }}></div>;
-}
