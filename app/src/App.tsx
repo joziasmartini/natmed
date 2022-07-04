6426824749b25212bd68bcc4495224a9fbb66ea5
Jozias Martini <jozias.martini@gmail.com>
2022-07-04 12:40:24 -0300

    Changes on App component to handle the event

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,15 +7,19 @@ import Button from "./components/Button/Button";
 import Navbar from "./components/Navbar/Navbar";
 import Space from "./components/Space/Space";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const clickHandle = (type: string) => {
+  const clickHandle = (type: string, event?: any) => {
     if (type === "login") {
       if (email === "jozias.martini@gmail.com" && password === "12345") {
         console.log("User logged in.");
+      } else {
+        event?.stopPropagation();
       }
     }
   };
@@ -27,7 +31,9 @@ function App() {
       <Version info={Package.version} />
       <Input type="text" placeholder="Email" action={setEmail} />
       <Input type="password" placeholder="Password" action={setPassword} />
-      <Button type="primary" text="Login" action={clickHandle} />
+      <Link to="/search">
+        <Button type="primary" text="Login" action={clickHandle} />
+      </Link>
       <Space pixels={120} />
       <Navbar theme="light" />
     </Interface>
