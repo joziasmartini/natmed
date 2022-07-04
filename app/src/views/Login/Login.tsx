5142fbc96cdcb30146ab73b4287572ca56365d55
Jozias Martini <jozias.martini@gmail.com>
2022-07-04 12:47:20 -0300

    Remove some unused components in Login component

--- app/src/views/Login/Login.tsx
+++ app/src/views/Login/Login.tsx
@@ -1,12 +1,10 @@
-import "Login.sass";
+import "./Login.sass";
 import Package from "../../../package.json";
 import Interface from "./../../components/Interface/Interface";
 import Logo from "./../../components/Logo/Logo";
 import Version from "./../../components/Version/Version";
 import Input from "./../../components/Input/Input";
 import Button from "./../../components/Button/Button";
-import Navbar from "./../../components/Navbar/Navbar";
-import Space from "./../../components/Space/Space";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -27,7 +25,6 @@ function Login() {
 
   return (
     <Interface>
-      <Space pixels={120} />
       <Logo />
       <Version info={Package.version} />
       <Input type="text" placeholder="Email" action={setEmail} />
@@ -35,8 +32,6 @@ function Login() {
       <Link to="/search">
         <Button type="primary" text="Login" action={clickHandle} />
       </Link>
-      <Space pixels={120} />
-      <Navbar theme="light" />
     </Interface>
   );
 }
