b49eb141b4f3ca4c36bfce61a491c24005326fb5
Jozias Martini <jozias.martini@gmail.com>
2022-07-18 22:02:02 -0300

    Implement link routing inside Button component

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
 import "./Button.sass";
 
 type Props = {
   text: string;
   type: "primary" | "secondary" | "danger";
   action: Function;
+  linkTo: string;
 };
 
-export default function Button({ text, type, action }: Props) {
+export default function Button({ text, type, action, linkTo }: Props) {
   return (
-    <div 
-      className={`button ${type}`} 
-      onClick={(event) => action("login", event)}
-    >
-      <span className="text">{text}</span>
-    </div>
+    <Link to={linkTo}>
+      <div 
+        className={`button ${type}`} 
+        onClick={(event) => action("login", event)}
+      >
+        <span className="text">{text}</span>
+      </div>
+    </Link>
   );
 }
