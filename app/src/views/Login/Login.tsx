664657fc0038b5e002d30a9b92a84d59333aabd0
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 15:32:56 -0300

    Add Space component and change div to fragment

--- app/src/views/Login/Login.tsx
+++ app/src/views/Login/Login.tsx
@@ -7,6 +7,7 @@ import Version from "./../../components/Version/Version";
 import Input from "./../../components/Input/Input";
 import Button from "./../../components/Button/Button";
 import { useNavigate } from "react-router-dom";
+import Space from "../../components/Space/Space";
 
 function Login() {
   const navigate = useNavigate();
@@ -28,16 +29,16 @@ function Login() {
 
   return (
     <Interface>
-      <div>{/* Empty element to fit on interface spacing configuration */}</div>
-      <div>
+      <Space pixels={0} />
+      <>
         <Logo />
         <Version info={Package.version} />
         <Input type="text" placeholder="Email" action={setEmail} />
         <Input type="password" placeholder="Password" action={setPassword} />
         {accountError && <span className="login-error">Your email or password is incorrect.</span>}
         <Button type="primary" text="Login" action={clickHandle} />
-      </div>
-      <div>{/* Empty element to fit on interface spacing configuration */}</div>
+      </>
+      <Space pixels={0} />
     </Interface>
   );
 }
