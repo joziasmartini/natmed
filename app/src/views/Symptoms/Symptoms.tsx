bcde222690ecad254fbdfa3046a59a7d264aa7d9
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 16:14:27 -0300

    Use Tag component inside Symtoms route

--- app/src/views/Symptoms/Symptoms.tsx
+++ app/src/views/Symptoms/Symptoms.tsx
@@ -6,6 +6,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import SearchInput from "../../components/SearchInput/SearchInput";
 import TopBar from "../../components/TopBar/TopBar";
 import Button from "../../components/Button/Button";
+import Tags from "../../components/Tags/Tags";
 
 function Symptoms() {
   const [symptoms, setSymptoms] = useState([]);
@@ -16,6 +17,7 @@ function Symptoms() {
           <TopBar back />
         </Link>
         <SearchInput saveSymptoms={setSymptoms} savedSymptoms={symptoms}/>
+        <Tags tags={symptoms}/>
         <Link to="/diagnosis">
           <Button type="primary" text="Diagnosis" action={() => {}} />
         </Link>
