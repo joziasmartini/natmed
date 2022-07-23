a371409d257aa5f7954b3fb4279e82b2e457fcba
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 17:15:23 -0300

    Make DiseaseCard receive a disease as a prop

--- app/src/components/DiseaseCard/DiseaseCard.tsx
+++ app/src/components/DiseaseCard/DiseaseCard.tsx
@@ -1,11 +1,13 @@
 import "./DiseaseCard.sass";
 
-type Props = {}
+type Props = {
+  disease: string; // disease to display
+}
 
-export default function DiseaseCard({}: Props) {
+export default function DiseaseCard({disease}: Props) {
   return (
     <div className="disease-card">
-      <img className="disease-card-image" src="https://source.unsplash.com/60x60" alt="Unsplash Image" />
+      <img className="disease-card-image" src="https://source.unsplash.com/60x60" alt="" />
       <h2 className="disease-card-name">Disease name</h2>
       <p className="disease-card-description">Disease description comes here with more words and a...</p>
       <div className="disease-card-tag-container">
