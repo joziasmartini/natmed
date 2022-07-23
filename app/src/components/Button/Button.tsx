152654f1da7e5c62829e77e8cbaecdef010441b8
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 20:25:19 -0300

    Style Button to support disabled state

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -5,16 +5,22 @@ type Props = {
   type: "primary" | "secondary" | "danger"; // Type of button to apply the styles  
   action: Function; // Function to be called when the button is clicked
   width?: number; // Max width of the button
+  disabled?: boolean; // If the button is disabled
 };
 
-export default function Button({ text, type, action, width }: Props) {
+export default function Button({ text, type, action, width, disabled }: Props) {
   return (
-    <div 
-      className={`button button--${type}`} 
+    <button 
+      className={`
+        button 
+        button--${type} 
+        button--${disabled ? "disabled" : ""}
+      `}
       onClick={(event) => action("login", event)}
       style={{ width: width }}
+      disabled={disabled}
     >
       <span className="text">{text}</span>
-    </div>
+    </button>
   );
 }
