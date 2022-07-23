4effeb747304c0c274faa80b56253d3523c1203c
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 18:48:11 -0300

    Restructure and style the DiseaseCard

--- app/src/components/DiseaseCard/DiseaseCard.tsx
+++ app/src/components/DiseaseCard/DiseaseCard.tsx
@@ -1,3 +1,4 @@
+import Button from "../Button/Button";
 import "./DiseaseCard.sass";
 
 type Props = {
@@ -6,12 +7,25 @@ type Props = {
 
 export default function DiseaseCard({disease}: Props) {
   return (
-    <div className="disease-card">
-      <img className="disease-card-image" src="https://source.unsplash.com/60x60" alt="" />
-      <h2 className="disease-card-name">Disease name</h2>
-      <p className="disease-card-description">Disease description comes here with more words and a...</p>
-      <div className="disease-card-tag-container">
-        <span className="disease-card-tag-item">Tag</span>
+    <div>
+      <div className="disease-container">
+        <div className="disease-card">
+          <img className="disease-card-image" src="https://source.unsplash.com/60x60" alt="" />
+          <div className="disease-card-text">
+            <h2 className="disease-card-name">Asma</h2>
+            <p className="disease-card-description">Uma inflamação das vias aéreas que persiste durante muito tempo</p>
+          </div>
+        </div>
+        <div className="disease-card-tag-container">
+          <span className="disease-card-tag-item">Falta de ar</span>
+          <span className="disease-card-tag-item">Dor no peito</span>
+          <span className="disease-card-tag-item">Sudorese</span>
+          <span className="disease-card-tag-item">Lábios azulados</span>
+        </div>
+      </div>
+      <div className="disease-card-buttons">
+        <a href="#" className="disease-card-description-button">Descrição</a>
+        <a href="#" className="disease-card-treatment-button">Tratamentos</a>
       </div>
     </div>
   )
