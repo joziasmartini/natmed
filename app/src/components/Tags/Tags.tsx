5c205b434ab1380f1e6617586450ce55278ee240
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 16:14:11 -0300

    Create Tag component to render visually selected symtoms

---
+++ app/src/components/Tags/Tags.tsx
@@ -0,0 +1,20 @@
+import './Tags.sass'
+
+type Props = {
+  tags: string[]; // tags to display
+}
+
+export default function Tags({tags}: Props) {
+  return (
+    <div>
+      <p>Sintomas: </p>
+      <div className="tag-container">
+        {
+          tags.map(tag => {
+            return <span className="tag-item">{tag}</span>;
+          })
+        }
+      </div>
+    </div>
+  )
+}
