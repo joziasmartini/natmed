dcd9acf733b1cb1b1b5a9430f573f56176906a96
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 15:33:09 -0300

    Insert comment on Button props

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
 import "./Button.sass";
 
 type Props = {
-  text: string;
-  type: "primary" | "secondary" | "danger";
-  action: Function;
+  text: string; // The text to display on the button
+  type: "primary" | "secondary" | "danger"; // Type of button to apply the styles  
+  action: Function; // Function to be called when the button is clicked
 };
 
 export default function Button({ text, type, action }: Props) {
