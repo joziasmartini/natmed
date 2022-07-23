a3cd1b541bfaaf29525d593e74532d8c18e202d1
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 17:30:48 -0300

    Add width and hover styling to Button

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -4,13 +4,15 @@ type Props = {
   text: string; // The text to display on the button
   type: "primary" | "secondary" | "danger"; // Type of button to apply the styles  
   action: Function; // Function to be called when the button is clicked
+  width?: number; // Max width of the button
 };
 
-export default function Button({ text, type, action }: Props) {
+export default function Button({ text, type, action, width }: Props) {
   return (
     <div 
       className={`button button--${type}`} 
       onClick={(event) => action("login", event)}
+      style={{ width: width }}
     >
       <span className="text">{text}</span>
     </div>
