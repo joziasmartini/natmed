5ca6061790f57244db9ef00281024d5bce72e45e
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 16:03:49 -0300

    Make Symptoms handle the state of SearchInput

--- app/src/views/Symptoms/Symptoms.tsx
+++ app/src/views/Symptoms/Symptoms.tsx
@@ -1,18 +1,21 @@
 import "./Symptoms.sass";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Interface from "../../components/Interface/Interface";
 import Navbar from "../../components/Navbar/Navbar";
 import SearchInput from "../../components/SearchInput/SearchInput";
 import TopBar from "../../components/TopBar/TopBar";
-import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 
-function Search() {
+function Symptoms() {
+  const [symptoms, setSymptoms] = useState([]);
+
   return (
     <Interface>
         <Link to="/login">
           <TopBar back />
         </Link>
-        <SearchInput />
+        <SearchInput saveSymptoms={setSymptoms} savedSymptoms={symptoms}/>
         <Link to="/diagnosis">
           <Button type="primary" text="Diagnosis" action={() => {}} />
         </Link>
@@ -21,4 +24,4 @@ function Search() {
   );
 }
 
-export default Search;
+export default Symptoms;
