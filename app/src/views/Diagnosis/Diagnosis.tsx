f3e3fddd1719896ccf5c18e40694d747e200924b
Jozias Martini <jozias.martini@gmail.com>
2022-07-23 20:57:58 -0300

    Small adjustment on imports

--- app/src/views/Diagnosis/Diagnosis.tsx
+++ app/src/views/Diagnosis/Diagnosis.tsx
@@ -1,9 +1,9 @@
 import "./Diagnosis.sass"
+import { Link } from "react-router-dom";
 import Interface from "../../components/Interface/Interface";
 import NavBar from "../../components/NavBar/NavBar";
 import TopBar from "../../components/TopBar/TopBar";
 import DiseaseCard from "../../components/DiseaseCard/DiseaseCard";
-import { Link } from "react-router-dom";
 
 export default function Diagnosis() {
   return (
